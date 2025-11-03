7dc143b1b50d5d2212359773e650744b555df579
Deyruiz <deivysdaniels@gmail.com>
2025-11-03 00:58:24 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_URL);
-    console.log(`✅ MongoDB conectado: ${conn.connection.host}`);
+    await mongoose.connect("mongodb+srv://Admin:Deyruiz1997@cluster0.34rpw1w.mongodb.net/SorteosOnline?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("✅ Conectado correctamente a MongoDB Atlas");
   } catch (error) {
-    console.error(`❌ Error al conectar DB: ${error.message}`);
+    console.error("❌ Error al conectar a MongoDB:", error.message);
     process.exit(1);
   }
 };
